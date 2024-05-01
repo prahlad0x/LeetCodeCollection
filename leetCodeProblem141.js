812111ce83e8a3f521129c961b08ad3fb323e2d0
Prahlad Kushwah <prahladkush231@gmail.com>
2024-05-01 14:22:49 +0530

    leetCode problem no. 141

---
+++ leetCodeProblem141.js
@@ -0,0 +1,64 @@
+problemUrl = `https://leetcode.com/problems/linked-list-cycle/`
+
+problemStatement = `
+    Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+    Return true if there is a cycle in the linked list. Otherwise, return false.
+
+    
+
+    Example 1:
+    Input: head = [3,2,0,-4], pos = 1
+    Output: true
+    Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+    Example 2:
+    Input: head = [1,2], pos = 0
+    Output: true
+    Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+    Example 3:
+    Input: head = [1], pos = -1
+    Output: false
+    Explanation: There is no cycle in the linked list.
+    
+
+    Constraints:
+
+    The number of the nodes in the list is in the range [0, 10^4].
+    -10^5 <= Node.val <= 10^5
+    pos is -1 or a valid index in the linked-list.
+    
+
+    Follow up: Can you solve it using O(1) (i.e. constant) memory?
+`
+
+// solution
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head || !head.next) return false
+
+    let slow = head
+    let fast = head.next
+
+    while(slow != fast){
+        if(!fast || !fast.next) return false
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return true
+};
